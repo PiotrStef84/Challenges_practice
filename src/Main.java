ac862c392c26067fba9544052a5eb00abfe85105
Piotr <xmestre@wp.pl>
2023-05-20 17:27:00 +0100

    twoNumberSum

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,5 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        "array": [3, 5, -4, 8, 11, 1, -1, 6],
-//        "targetSum": 10
-
-        int[] testArray = {3,5,-4,8,11,1,-1,6};
-        int targetSum = 10;
-        int [] result = twoNumberSum(testArray, targetSum);
-
-        System.out.println(Arrays.toString(result));
-
-
-    }
-    public static int[] twoNumberSum(int[] array, int targetSum) {
-        // Write your code here.
-        int[] target = new int[2];
-        for (int i = 0; i < array.length; i++){
-            for(int j =0; j < array.length; j++){
-                if(j + i == targetSum){
-                    target[0] = i;
-                    target[1] = j;
-                    return target;
-                }
-            }
-        }
-        return new int[0];
     }
 }
