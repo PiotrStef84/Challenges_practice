162a81796bfcf9e02ca168728aeefb251744a26b
Piotr <xmestre@wp.pl>
2023-05-29 12:35:05 +0100

    Adding second solution to TwoNumSum that improves time complexity

--- src/Easy/TwoNumSum.java
+++ src/Easy/TwoNumSum.java
@@ -5,6 +5,14 @@ import java.util.Arrays;
 public class TwoNumSum {
     public static void main(String[] args) {
 
+
+    /* Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. if
+    any two numbers in the input array sum up to the target sum, the function should return them in an array, in any
+    order.
+
+    If no two numbers sum to the target sum, the function should return an empty array */
+
+
         //        "array": [3, 5, -4, 8, 11, 1, -1, 6],
         //        "targetSum": 10
 
@@ -12,11 +20,18 @@ public class TwoNumSum {
         int targetSum = 10;
         int [] result = twoNumberSum(testArray, targetSum);
 
+        int [] result2 = twoNumberSumPointers(testArray, targetSum);
+
+        //System.out.println(Arrays.toString(result));
+
         System.out.println(Arrays.toString(result));
     }
 
+
+    //  Brute force solution
+    // O(n^2) time -> looping twice through the array | O(1) space
     public static int[] twoNumberSum(int[] array, int targetSum) {
-        // Write your code here.
+
         int[] target = new int[2];
         for (int i = 0; i < array.length; i++){
             for(int j =0; j < array.length; j++){
@@ -25,6 +40,27 @@ public class TwoNumSum {
                     target[1] = array[j];
                     return target;
                 }
+            }
+        }
+        return new int[0];
+    }
+    // Second solution - > using pointers instead of two loops, better time complexity.
+    // O(nlog(n)) time | O(1) space
+    public static int[] twoNumberSumPointers(int[] array, int targetSum){
+
+        Arrays.sort(array);
+
+        int left = 0;
+        int right = array.length - 1;
+
+        // if left reaches right, the pointers met and the array is traversed
+        while(left < right){
+            if(array[left] + array[right] == targetSum){
+                return new int[] {array[left], array[right]};
+            } else if (array[left] + array[right] < targetSum){
+                left += 1;
+            } else if (array[left] + array[right] > targetSum) {
+                right -=  1;
             }
         }
         return new int[0];
