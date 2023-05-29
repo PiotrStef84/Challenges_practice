7cae34e6d2c72bca3e249cf22822ea551e022da9
Piotr <xmestre@wp.pl>
2023-05-29 15:45:33 +0100

    Refactoring to add String challenges

--- src/Arrays/Easy/SortedSquaredArray.java
+++ src/Arrays/Easy/SortedSquaredArray.java
@@ -1,8 +1,6 @@
-package Easy;
+package Arrays.Easy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class SortedSquaredArray {
     public static void main(String[] args) {
---
+++ src/Strings/Easy/PalindromeCheck.java
@@ -0,0 +1,14 @@
+package Strings.Easy;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("abcdcba"));
+        System.out.println(isPalindrome("abcdefghihgfeddcba"));
+    }
+
+    public static boolean isPalindrome(String str) {
+        // Write your code here.
+        return false;
+    }
+}
