2dde0b5b7919eb76fed193d739cfa7651cd6b167
Piotr <xmestre@wp.pl>
2023-05-30 09:31:08 +0100

    Added Palindrome Check

--- src/Strings/Easy/PalindromeCheck.java
+++ src/Strings/Easy/PalindromeCheck.java
@@ -5,10 +5,22 @@ public class PalindromeCheck {
 
         System.out.println(isPalindrome("abcdcba"));
         System.out.println(isPalindrome("abcdefghihgfeddcba"));
+        System.out.println(isPalindrome("zzaaoaazz"));
     }
 
+    // Solution using pointers
+    // O(n) time | O(1) space  --> just iterating once through the String [array of chars], no new data structure created
     public static boolean isPalindrome(String str) {
-        // Write your code here.
-        return false;
+        int leftIdx = 0;
+        int rightIdx = str.length() - 1;
+
+        while(leftIdx < rightIdx){
+            if(str.charAt(leftIdx) != str.charAt(rightIdx)){
+                return false;
+            }
+            leftIdx++;
+            rightIdx--;
+        }
+        return true;
     }
 }
